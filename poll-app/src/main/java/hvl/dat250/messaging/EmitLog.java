83b5e9e1eaffe11f5c5b984e94422c7f91b99aa2
Vojtěch Luňák <vojtech.lunak1@gmail.com>
2024-11-01 18:22:25 +0100

    feat(messaging): docker-compose, dockerfiles, application-properties; rabbitmq does not create queue and fails; neo4j connection not working;

--- poll-app/src/main/java/hvl/dat250/messaging/EmitLog.java
+++ poll-app/src/main/java/hvl/dat250/messaging/EmitLog.java
@@ -1,17 +1,24 @@
 package hvl.dat250.messaging;
 
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.core.Queue;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmitLog {
 
     private static final String EXCHANGE_NAME = "poll-data-exchange";
-    private static final String ROUTING_KEY = "poll-data";
+    private static final String QUEUE_NAME = "poll-data";
+    private static final String ROUTING_KEY = "poll-data-1";
 
     public static void main(String[] argv) throws Exception {
         SpringApplication.run(EmitLog.class, argv);
@@ -20,13 +27,10 @@ public class EmitLog {
     @Bean
     public ApplicationRunner runner(RabbitTemplate template) {
         return args -> {
+            ApplicationContext context = new AnnotationConfigApplicationContext(RabbitMQConfig.class);
+            AmqpTemplate amqpTemplate = context.getBean(AmqpTemplate.class);
             System.out.println("Sending message...");
-            template.convertAndSend(ROUTING_KEY, "Poll::" + System.currentTimeMillis());
+            amqpTemplate.convertAndSend( "Poll::" + System.currentTimeMillis());
         };
-    }
-
-    @Bean
-    public Queue myQueue() {
-        return new Queue(ROUTING_KEY, false);
     }
 }
