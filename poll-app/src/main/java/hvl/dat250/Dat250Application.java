b2629dc5be9a0e32db01a283fd34bcb3af7a15a5
Vojtěch Luňák <vojtech.lunak1@gmail.com>
2024-11-24 14:25:50 +0100

    feat(docker-compose): full app deployment working; frontend running on port 80; docs todo; messaging service todo

--- poll-app/src/main/java/hvl/dat250/Dat250Application.java
+++ poll-app/src/main/java/hvl/dat250/Dat250Application.java
@@ -3,11 +3,11 @@ package hvl.dat250;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-//public class Dat250Application {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(Dat250Application.class, args);
-//	}
-//
-//}
+@SpringBootApplication
+public class Dat250Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dat250Application.class, args);
+	}
+
+}
--- poll-app/src/main/java/hvl/dat250/messaging/EmitLog.java
+++ poll-app/src/main/java/hvl/dat250/messaging/EmitLog.java
@@ -5,20 +5,22 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+
+@Configuration
 public class EmitLog {
 
-    public static void main(String[] argv) throws Exception {
-        SpringApplication.run(EmitLog.class, argv);
-    }
-
-    @Bean
-    public ApplicationRunner runner(RabbitTemplate template) {
-        return args -> {
-            String message = "Poll::" + System.currentTimeMillis();
-            System.out.println("Sending message: " + message);
-            template.convertAndSend(RabbitMQConfig.QUEUE_NAME,  message);
-        };
-    }
+//    public static void main(String[] argv) throws Exception {
+//        SpringApplication.run(EmitLog.class, argv);
+//    }
+//
+//    @Bean
+//    public ApplicationRunner runner(RabbitTemplate template) {
+//        return args -> {
+//            String message = "Poll::" + System.currentTimeMillis();
+//            System.out.println("Sending message: " + message);
+//            template.convertAndSend(RabbitMQConfig.QUEUE_NAME,  message);
+//        };
+//    }
 }
